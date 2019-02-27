8f39d7c2d098fe3027cd7218782f090e9ebcaca4
sp6pe <sp6pe@virginia.edu>
2019-02-27 18:38:40 +0000

    events and fcn call to stream

--- lib/predicates.ts
+++ lib/predicates.ts
@@ -166,3 +166,69 @@ export class ToAddress extends Predicate {
     return data.TX.TOADDR == this.addr;
   } 
 }
+
+export class EventSmartContractAddress extends Predicate {
+  private addr: string
+
+  constructor(addr: string) {
+    super()
+    this.addr = addr;
+  }
+
+  isTrue(data: any): boolean {
+      return data.addresss == this.addr;
+  } 
+}
+
+export class FunctionCallSmartContractAddress extends Predicate {
+  private addr: string
+
+  constructor(addr: string) {
+    super()
+    this.addr = addr;
+  }
+
+  isTrue(data: any): boolean {
+      return data.callTo == this.addr;
+    }
+  } 
+}
+
+export class SignatureHash extends Predicate {
+  private hash: string
+
+  constructor(hash: string) {
+    super()
+    this.hash = hash;
+  }
+
+  isTrue(data: any): boolean {
+    return data.signatureHash == this.hash;
+  } 
+}
+
+export class EventName extends Predicate {
+  private event: string
+
+  constructor(event: string) {
+    super()
+    this.event = event;
+  }
+
+  isTrue(data: any): boolean {
+    return data.event == this.event;
+  } 
+}
+
+export class FunctionName extends Predicate {
+  private functionName: string
+
+  constructor(functionName: string) {
+    super()
+    this.functionName = functionName;
+  }
+
+  isTrue(data: any): boolean {
+    return data.function == this.functionName;
+  } 
+}
